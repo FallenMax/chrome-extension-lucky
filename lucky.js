f7c55818e71886068f62e713238c85f485f6365f
FallenMax <fallenmax@gmail.com>
2015-12-13 02:15:09 +0800

    init

---
+++ lucky.js
@@ -0,0 +1,13 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    title: 'I\'m Feeling Lucky',
+    contexts:['selection'],
+    id: 'feeling-lucky'})
+})
+
+chrome.contextMenus.onClicked.addListener(function(info) {
+  chrome.tabs.create({
+    url: 'http://www.google.com/search?q=' + info.selectionText +'&btnI=Im+Feeling+Lucky',
+    active: false
+  })
+})
