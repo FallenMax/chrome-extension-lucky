8ed789b82313d148965b3e7cdf6ebec624bf951e
FallenMax <FallenMax@gmail.com>
2024-09-16 09:52:20 +0800

    migrate to manifest v3

--- lucky.js
+++ lucky.js
@@ -1,14 +1,14 @@
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function () {
   chrome.contextMenus.create({
-    title: 'I\'m Feeling Lucky',
-    contexts:['selection'],
-    id: 'feeling-lucky'
+    title: "I'm Feeling Lucky",
+    contexts: ['selection'],
+    id: 'feeling-lucky',
   })
 })
 
-chrome.contextMenus.onClicked.addListener(function(info) {
+chrome.contextMenus.onClicked.addListener(function (info) {
   chrome.tabs.create({
-    url: 'http://www.google.com/search?q=' + info.selectionText +'&btnI=Im+Feeling+Lucky',
-    active: false
+    url: 'https://www.google.com/search?q=' + encodeURIComponent(info.selectionText) + '&btnI=Im+Feeling+Lucky',
+    active: false,
   })
 })
